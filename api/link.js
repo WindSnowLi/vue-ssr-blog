03bbd25720d9ba061681ac9067a6835a5092af53
WindSnowLi <wzmmdsnn@163.com>
2021-08-07 14:56:22 +0800

    初步添加友链

---
+++ api/link.js
@@ -0,0 +1,31 @@
+import request from '../utils/request.js';
+
+/**
+ *  获取友链列表
+ */
+export function getFriendLinks() {
+  return request({
+    url: '/links/getFriendLinks',
+    method: 'post',
+    data: {
+      status: 'PASS'
+    }
+  })
+}
+
+/**
+ *  申请友链
+ */
+export function applyFriendLink(friendLinks) {
+  return request({
+    url: '/links/applyFriendLink',
+    method: 'post',
+    data: {
+      coverPic: friendLinks.coverPic,
+      email: friendLinks.email,
+      describe: friendLinks.describe,
+      title: friendLinks.title,
+      link: friendLinks.link
+    }
+  })
+}
---
+++ pages/links.vue
@@ -0,0 +1,20 @@
+<template>
+  <frend-links :frend-links="links"></frend-links>
+</template>
+
+<script>
+  import {
+    getFriendLinks
+  } from '@/api/link.js'
+  export default {
+    layout: 'index',
+    async asyncData() {
+      let [links] = await Promise.all([
+        getFriendLinks()
+      ])
+      return {
+        links
+      }
+    },
+  }
+</script>

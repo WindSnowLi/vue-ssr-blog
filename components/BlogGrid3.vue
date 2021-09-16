982f77abd06bfa96f0ccaac4061fb217acab1e60
WindSnowLi <wzmmdsnn@163.com>
2021-09-16 12:39:32 +0800

    add page

--- components/BlogGrid3.vue
+++ components/BlogGrid3.vue
@@ -5,25 +5,25 @@
         <!-- 文章 -->
         <div class="post-default">
           <div class="post-thumb">
-            <nuxt-link :to="{ name:'article-detail-id',params:{ id: article.article.id } }">
-              <img :src="article.article.coverPic" alt="" class="img-fluid">
+            <nuxt-link :to="{ name:'article-detail-id',params:{ id: article.id } }">
+              <img :src="article.coverPic" alt="" class="img-fluid">
             </nuxt-link>
           </div>
           <div class="post-data">
             <!-- 分类 -->
-            <div class="cats"><a href="javascript:void(0)">{{ article.article.articleType.name }}</a></div>
+            <div class="cats"><a href="javascript:void(0)">{{ article.articleType.name }}</a></div>
             <!-- Title -->
             <div class="title">
               <h2>
-                <nuxt-link :to="{ name:'article-detail-id',params:{ id: article.article.id } }">
-                  {{ article.article.title }}
+                <nuxt-link :to="{ name:'article-detail-id',params:{ id: article.id } }">
+                  {{ article.title }}
                 </nuxt-link>
               </h2>
             </div>
             <!-- 描述 -->
             <div class="desc">
               <p>
-                {{ article.article.summary.substring(0,50) }}···
+                {{ article.summary.substring(0,50) }}
               </p>
             </div>
           </div>
@@ -50,7 +50,7 @@ export default {
   watch: {
     articles() {
       this.$nextTick(() => { // 在dom渲染完后,再执行动画
-         		new WOW().init()
+        new WOW().init()
       })
     }
   }
--- pages/article/type/_id.vue
+++ pages/article/type/_id.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
-    <article-type :articles="articles" :label="label"></article-type>
+    <article-type :articles="articles" :label="label" />
     <!-- 分页 -->
-    <paging :total="total" :jump="getArticlesByType" :limit="limit"></paging>
+    <paging :total="total" :jump="getArticlesByType" :limit="limit" />
     <!-- 分页结束 -->
   </div>
 </template>
 
 <script>
-import {getVisitorInfo} from "../../../api/user";
-import {getArticlesByType, getHistoryArticles, getTypeById} from "../../../api/article";
+import { getVisitorInfo } from '../../../api/user'
+import { getArticlesByType, getTypeById } from '../../../api/article'
 
 export default {
   layout: 'index',
   async asyncData({
-                    params
-                  }) {
-    let limit = 15
-    let [user, articles, label] = await Promise.all([
+    params
+  }) {
+    const limit = 15
+    const [user, articles, label] = await Promise.all([
       getVisitorInfo(),
       getArticlesByType(params.id, 1, limit),
-      getTypeById(params.id),
+      getTypeById(params.id)
     ])
     return {
       user,
@@ -30,6 +30,15 @@ export default {
       limit
     }
   },
+  methods: {
+    getArticlesByType(page) {
+      const _self = this
+      getArticlesByType(this.$route.params.id, page, _self.limit).then((data) => {
+        _self.articles = data.items
+        _self.total = data.total
+      })
+    }
+  },
   head() {
     return {
       title: '首页-分类-' + this.label.name,
@@ -37,24 +46,15 @@ export default {
         name: 'keywords',
         content: this.label.name
       },
-        {
-          name: 'author',
-          content: this.user.nickname
-        },
-        {
-          name: 'description',
-          content: this.label.describe || this.user.nickname + '的' + this.label.name + '文章'
-        }
+      {
+        name: 'author',
+        content: this.user.nickname
+      },
+      {
+        name: 'description',
+        content: this.label.describe || this.user.nickname + '的' + this.label.name + '文章'
+      }
       ]
-    }
-  },
-  methods: {
-    getArticlesByType(page) {
-      const _self = this
-      getArticlesByType(this.$route.params.id, page, _self.limit).then(data => {
-        _self.articles = data.items
-        _self.total = data.total
-      })
     }
   }
 }
